258d2d4aa6a95beeb9c78170f803dbd0b6a36a62
Vicompte <96453560+Vicompte@users.noreply.github.com>
2025-06-25 03:51:16 +0200

    Correction des exports cassés dans src/components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
-export { default as AdminPanel } from './AdminPanel';
-export { default as LoginForm } from './LoginForm';
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as LoginPage } from './LoginPage';
-export { default as ClientPortal } from './ClientPortal';
-export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as AdminPanel } from './AdminPanel'
+export { default as AdminDashboard } from './AdminDashboard'
+export { default as ClientPortal } from './ClientPortal'
+export { default as ProtectedRoute } from './ProtectedRoute'
+export { default as Login } from './Login'
